62f4197591a09c7e9d075340b03c1f498b2a31ce
unotrailblazer <lmysakh@gmail.com>
2023-09-07 16:04:06 +0530

    Latest commit

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -8,7 +8,7 @@ import { search } from '@syncfusion/ej2/filemanager'
 
 const Employees = () => {
   return (
-    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+    <div className='m-2 md:m-10 mt-24 p-2  md:p-10 bg-white rounded-3xl'>
       <Header category="Page" title="Employees" />
       <GridComponent
         dataSource={employeesData}
